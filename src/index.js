7ee3da737aa6931e44a42b24e418b3655a08b63f
seunjay <seunjay92@gmail.com>
2023-02-17 14:50:55 +0100

    implementing google oauth login

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <GoogleOAuthProvider
+        clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+      >
+        <App />
+      </GoogleOAuthProvider>
+    </Router>
   </React.StrictMode>
 );
 
