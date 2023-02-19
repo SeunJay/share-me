a680b431e5964e64f3ab75e34132e4398f044571
seunjay <seunjay92@gmail.com>
2023-02-19 15:58:01 +0100

    create pin component

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Feed } from "./Feed";
 export { default as PinDetails } from "./PinDetails";
 export { default as CreatePin } from "./CreatePin";
 export { default as Search } from "./Search";
+export { default as MasonryLayout } from "./MasonryLayout";
